e783615186cb9cb36311b3cc4202fdabc72ad902
IPF . <nazagarcia132@gmail.com>
2024-05-13 12:30:00 -0300

    termine los 3 ejercicios

---
+++ assets/js/numParImpar.js
@@ -0,0 +1,9 @@
+
+for (let number = 0; number <= 10; number++) {
+    if (number % 2 == 0) {
+      console.log("Número par");
+    } else {
+      console.log("Número impar");
+    }
+  }
+  
---
+++ assets/js/triangulo.js
@@ -0,0 +1,6 @@
+
+let t = '';
+for (let i = 1; i <= 5; i++) {
+    t += '#';
+    console.log(t);
+}
